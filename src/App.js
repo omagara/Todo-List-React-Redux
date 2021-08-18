1bc52e4c93f3a633605c1ca22759fe9bb243abf0
omagara <omagara@oocl.com>
2021-08-18 14:47:34 +0800

    [TodoList][Ramon]chore: added dependencies

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import './App.css';
 import TodoList from './features/todos/components/TodoList';
-
+import {Route, Link, BrowserRouter, Switch} from 'react-router-dom';
+import NotFoundPage from './features/notfound/NotFoundPage';
+import DoneList from './features/todos/components/DoneList';
 function App() {
   return (
     <div className="App">
-      <TodoList/>
+      <BrowserRouter>
+      <ul>
+        <Link to = "/todo">TodoList link</Link>
+        <Link to = "/donelist">DoneList link</Link>
+      </ul>
+      <Switch>
+        <Route exact path ="/todo" component = {TodoList}></Route>
+        <Route exact path ="/donelist" component = {DoneList}></Route>
+        <Route path = "*" component = {NotFoundPage} ></Route>
+      </Switch>
+      </BrowserRouter>
     </div>
   );
 }
