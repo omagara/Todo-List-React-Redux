ab07b0a7bbe4caad1845d48821ecc2d084b88c12
omagara <omagara@oocl.com>
2021-08-18 21:07:08 +0800

    [TodoList][Ramon]feat: implemented getTodos API to retreive the Todo items

--- src/features/todos/components/TodoList.js
+++ src/features/todos/components/TodoList.js
@@ -1,15 +1,20 @@
 import React from 'react'
 import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import TodoForm from './TodoForm';
 import TodoGroup from './TodoGroup';
 import '../styles/TodoList.css'
 import { getTodos } from '../../apis/todos';
+import { AddTodos } from '../reducers/todosSlice';
 
 function TodoList() {
     
+    const dispatch = useDispatch();
+    
     useEffect(() => {
         getTodos().then((response) => {
-            console.log("response: ", response.data)
+            console.log("response: ", response.data);
+            dispatch(AddTodos(response.data));
         })
     }, []) 
     return (
