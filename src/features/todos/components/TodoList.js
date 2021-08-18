ca459a721dfa7e3e09252d0da75311277c0bbcc2
omagara <omagara@oocl.com>
2021-08-18 14:52:12 +0800

    [TodoList][Ramon]feat: implemented useEffect on TodoList.js

--- src/features/todos/components/TodoList.js
+++ src/features/todos/components/TodoList.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import { useEffect } from 'react';
 import TodoForm from './TodoForm';
 import TodoGroup from './TodoGroup';
 import '../styles/TodoList.css'
+import { getTodos } from '../../apis/todos';
 
 function TodoList() {
+    
+    useEffect(() => {
+        getTodos().then((response) => {
+            console.log("response: ", response.data)
+        })
+    }, []) 
     return (
         <div className="todo-container">
             <h1>Todo List</h1>
