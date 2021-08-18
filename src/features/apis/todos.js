57d536f7c711bb608e840511444ee5dd150ab3b4
omagara <omagara@oocl.com>
2021-08-18 21:09:05 +0800

    [TodoList][Ramon]feat: added getTodos API

--- src/features/apis/todos.js
+++ src/features/apis/todos.js
@@ -3,3 +3,12 @@ import api from './api';
 export const getTodos = () =>{
     return api.get("/todos");
 }
+
+export const createTodo = () =>{
+    return api.post("/todos", {
+        id: "23",
+        text: "todo example",
+        done: false,
+      })
+}
+
