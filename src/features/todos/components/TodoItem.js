ccb7c1a32e5998cb7ae9c4b7f992d9b0b3b8faf8
omagara <omagara@oocl.com>
2021-08-18 21:45:37 +0800

    [TodoList][Ramon]feat: implemented updateTodo API for changing status of Todo items

--- src/features/todos/components/TodoItem.js
+++ src/features/todos/components/TodoItem.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { selectTodoById, ToggleTodo, RemoveTodo } from '../reducers/todosSlice';
 import { useSelector, useDispatch } from 'react-redux';
 import '../styles/TodoItem.css'
+import { updateTodo } from '../../apis/todos';
 
 function TodoItem(props) {
     const todo = useSelector(state => selectTodoById(state, props.itemId))
@@ -9,16 +10,16 @@ function TodoItem(props) {
     const todoStatus = todo.done ? "done" : "";
     
     function handleClick(){
-        dispatch(ToggleTodo(props.itemId))
-    }
+        updateTodo(props.itemId, {done: !todo.done}).then((response) => {
+            dispatch(ToggleTodo({id: props.itemId, updateTodo:response.data}));
+        });  
+    };
 
     function handleRemove(event){
         event.stopPropagation();
         dispatch(RemoveTodo(props.itemId));
     }
 
-    
-    
     return (
         <div className="items">
             <div className = {`TodoItem-todo ${todoStatus}`} onClick={handleClick}>
