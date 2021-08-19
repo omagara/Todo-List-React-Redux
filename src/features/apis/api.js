83a786e1dc08422feee2f4070bfb01b63441271d
omagara <omagara@oocl.com>
2021-08-19 15:49:18 +0800

    [TodoList][Ramon]feat: modified the styles for the TodoList, TodoItem, and App

--- src/features/apis/api.js
+++ src/features/apis/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: 'https://611cc23ca18e850017decc47.mockapi.io/'
+    // baseURL: 'https://611cc23ca18e850017decc47.mockapi.io/'
+    baseURL: 'http://localhost:8090'
 
 });
 
