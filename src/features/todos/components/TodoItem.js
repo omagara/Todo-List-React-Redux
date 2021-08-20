83e0b89ccdaf4d125ec967d3f49513a5f7ae6f7f
omagara <omagara@oocl.com>
2021-08-20 08:49:30 +0800

    [TodoList][Ramon]feat: implemented the edit item of TodoList

--- src/features/todos/components/TodoItem.js
+++ src/features/todos/components/TodoItem.js
@@ -1,10 +1,10 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { selectTodoById, ToggleTodo, RemoveTodo } from '../reducers/todosSlice';
 import { useSelector, useDispatch } from 'react-redux';
 import '../styles/TodoItem.css'
 import { deleteTodo, updateTodo } from '../../apis/todos';
 import {DeleteOutlined, EditOutlined} from '@ant-design/icons'
-import { Modal, Button } from 'antd';
+import { Modal, Button, Input } from 'antd';
 
 function TodoItem(props) {
     const todo = useSelector(state => selectTodoById(state, props.itemId))
@@ -24,13 +24,50 @@ function TodoItem(props) {
         event.stopPropagation();
     }
 
+ 
+    const [isModalVisible, setIsModalVisible] = useState(false);
+    const [textInput, setTextInput] = useState(todo.text);
+    const { TextArea } = Input;    
+          
+            const showModal = () => {
+                if (todoStatus === todo.done){
+                    EditOutlined.visible(false);
+                    setIsModalVisible(false);
+                }
+                else {
+                setIsModalVisible(true);
+                }
+            
+          };
+          const handleOk = () => {
+              if (Input === todo.text || Input === ""){
+                setIsModalVisible(false);
+              }
+              else{
+                  setTextInput(Input);
+              }
+            
+          };
+        
+          const handleCancel = () => {
+            setIsModalVisible(false);
+          };
+        
+        
+
+
     return (
         <div className="items">
             <div className = {`TodoItem-todo ${todoStatus}`} onClick={handleClick}>
                     <span className="todoText">{todo.text}</span>
+                    </div>
                     <span className ="todoRemove" onClick={handleRemove}><DeleteOutlined /></span>
-                  
-            </div>
+                    <span className ="todoEdit" onClick={showModal}><EditOutlined /> </span>
+                       
+              <Modal title="Basic Modal" visible={isModalVisible} onOk={handleOk} onCancel={handleCancel}>
+                    <TextArea rows={4} defaultValue={todo.text}></TextArea>  
+              </Modal>
+            
         </div>
     )
 }
