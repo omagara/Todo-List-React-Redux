9f222c627510d92eca86d4538d4ad7989dd87b0b
omagara <omagara@oocl.com>
2021-08-21 14:31:56 +0800

    [TodoList][Ramon]feat: added css style on NotFoundPage

--- src/features/notfound/NotFoundPage.js
+++ src/features/notfound/NotFoundPage.js
@@ -1,9 +1,11 @@
 import React from 'react'
 
 function NotFoundPage() {
+    
+    
     return (
         <div>
-            <h1>Page Not Found</h1>
+            <h1 style={{marginTop: 250 + 'px', fontSize: 100 + 'px', color: 'black'}}>Page Not Found</h1>
         </div>
     )
 }
