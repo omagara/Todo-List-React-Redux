d41b07a77c1fab5712addaf835a5a4edbcbfc442
omagara <omagara@oocl.com>
2021-08-22 12:22:57 +0800

    [TodoList][Ramon]refactor: removed unused javasript files; constants.js and utils.js

--- src/common/constants/constants.js
+++
@@ -1,7 +0,0 @@
-export const initialTodoList = [
-    {
-    id: "cc53dc26-61b0-406b-99dd-b8825dd2ceec",
-    text: "todo example",
-    done: false,
-    },
-];
--- src/common/utils/utils.js
+++
@@ -1,7 +0,0 @@
-export function getAllTodoIds (todos){
-    return todos.map((todo) => todo.id);
-}
-
-export function getTodoById(todos, id){
-    return todos.find((todo)=>todo.id ===id)
-}
